eb3106faff365195678d9da52c002e92c3518ec3
Esoteric918 <3411@holbertonschool.com>
2022-07-06 11:58:20 -0500

    sandpiles

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -86,14 +86,31 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
     int i, j;
 
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
     while (unstable_grid(grid1))
     {
-        topple_grid(grid1, grid2);
         for (i = 0; i < 3; i++)
         {
             for (j = 0; j < 3; j++)
             {
-                grid1[i][j] = grid2[i][j];
+                if (grid1[i][j] > 3)
+                {
+                    grid1[i][j] -= 4;
+                    if (i > 0)
+                        grid1[i - 1][j]++;
+                    if (i < 2)
+                        grid1[i + 1][j]++;
+                    if (j > 0)
+                        grid1[i][j - 1]++;
+                    if (j < 2)
+                        grid1[i][j + 1]++;
+                }
             }
         }
     }
