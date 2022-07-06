ba3bc30710d945cd290544e25005a03b8925e924
Esoteric918 <3411@holbertonschool.com>
2022-07-06 11:12:39 -0500

    sandpiles

---
+++ 0x03-sandpiles/0-sandpiles.c
@@ -0,0 +1,100 @@
+#include "sandpiles.h"
+
+/**
+ * print_grid_sum - Print 3x3 grids sum
+ * @grid1: Left 3x3 grid
+ *
+ */
+void print_sp(int grid1[3][3])
+{
+    int i, j;
+
+    printf("=\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid1[i][j]);
+        }
+        printf("\n");
+    }
+}
+/**
+ * unstable_grid - Check if grid is unstable
+ * @grid: 3x3 grid
+ * Return: 1 if unstable, 0 if stable
+ */
+int unstable_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+            {
+                print_sp(grid);
+                return (1);
+            }
+
+        }
+    }
+    return (0);
+}
+/**
+ * topple_grid - toppled grid
+ * @grid1: 3x3 grid
+ *
+ * Return: void
+ */
+void topple_grid(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid2[i][j] = grid1[i][j] + grid1[i][j];
+            if (grid2[i][j] > 3)
+            {
+                grid2[i][j] -= 4;
+                if (i > 0)
+                    grid2[i - 1][j]++;
+                if (i < 2)
+                    grid2[i + 1][j]++;
+                if (j > 0)
+                    grid2[i][j - 1]++;
+                if (j < 2)
+                    grid2[i][j + 1]++;
+            }
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - Sum two 3x3 grids
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ * Return: void
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    while (unstable_grid(grid1))
+    {
+        topple_grid(grid1, grid2);
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                grid1[i][j] = grid2[i][j];
+            }
+        }
+    }
+}
