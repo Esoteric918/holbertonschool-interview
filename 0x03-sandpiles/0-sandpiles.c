8073d999f36369ec116eed263a90d4afaf289a41
Esoteric918 <3411@holbertonschool.com>
2022-07-08 12:11:27 -0500

    fix toppling remove unused func

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -44,37 +44,6 @@ int unstable_grid(int grid[3][3])
 	return (0);
 }
 /**
- * topple_grid - toppled grid
- * @grid1: 3x3 grid
- * @grid2: 3x3 grid
- * Return: void
- */
-void topple_grid(int grid1[3][3], int grid2[3][3])
-{
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			grid2[i][j] = grid1[i][j] + grid1[i][j];
-			if (grid2[i][j] > 3)
-			{
-				grid2[i][j] -= 4;
-				if (i > 0)
-					grid2[i - 1][j]++;
-				if (i < 2)
-					grid2[i + 1][j]++;
-				if (j > 0)
-					grid2[i][j - 1]++;
-				if (j < 2)
-					grid2[i][j + 1]++;
-			}
-		}
-	}
-}
-
-/**
  * sandpiles_sum - Sum two 3x3 grids
  * @grid1: Left 3x3 grid
  * @grid2: Right 3x3 grid
