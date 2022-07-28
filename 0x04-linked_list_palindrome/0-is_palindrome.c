275e06cb57b91be2d1f4abf547c4f9b3b96ff970
Esoteric918 <3411@holbertonschool.com>
2022-07-28 09:22:47 -0500

    is palindrome

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -1,56 +1,54 @@
 #include "lists.h"
 
 /**
+ * compare - compares two nodes
+ *
+ * @a - first node
+ * @b - second node
+ * Return int
+ */
+
+int compare(listint_t *a, listint_t *b)
+{
+    if (a == NULL && b == NULL)
+        return (1);
+    if (a == NULL || b == NULL)
+        return (0);
+    if (a->n == b->n)
+        return (compare(a->next, b->next));
+    return (0);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the first node of the list
  *
  * Return: 1 if it is a palindrome, 0 if it is not
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *current, *reverse, *temp;
-	int i, j, flag, count;
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *prev = NULL;
+    listint_t *next = NULL;
 
-	if (*head == NULL)
-		return (1);
-	current = *head;
-	reverse = NULL;
-	count = 0;
-	while (current != NULL)
-	{
-		count++;
-		current = current->next;
-	}
-	current = *head;
-	for (i = 0; i < count / 2; i++)
-	{
-		temp = current->next;
-		current->next = reverse;
-		reverse = current;
-		current = temp;
-	}
-	if (count % 2 == 0)
-		j = count / 2 - 1;
-	else
-		j = count / 2;
-	flag = 1;
-	while (j >= 0)
-	{
-		if (reverse->n != current->n)
-		{
-			flag = 0;
-			break;
-		}
-		reverse = reverse->next;
-		current = current->next;
-		j--;
-	}
-	if (flag == 1)
-	{
-		return (1);
-		printf("Linked list is a palindrome\n");
-	}
-	else
-		return (0);
+    if (head == NULL || *head == NULL)
+        return (1);
+    while (fast != NULL && fast->next != NULL)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast != NULL)
+        next = slow->next;
+    else
+        next = NULL;
+    slow->next = NULL;
+    prev->next = NULL;
+    if (next != NULL)
+        *head = next;
+    else
+        *head = NULL;
+    return (compare(*head, prev));
 }
