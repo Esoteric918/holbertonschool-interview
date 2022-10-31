61b4e5e8a952f561a505728c4b7d3eea555f6d01
esoteric918 <3411@holbertonstudent.com>
2022-10-31 10:27:36 -0500

    rotate function w/ main file

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """Rotate a 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
+    return (matrix[first], matrix[last])
+
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,24 @@
+# 0x07 Rotate 2D Matrix
+
+## Description
+    This is a program that rotates a 2D matrix by 90 degrees.
+
+## Requirements
+    - Ubuntu 20.04 LTS
+    - gcc 4.8.4
+    - Python 3.8.5
+
+## File
+    - 0-rotate_2d_matrix.py
+    - 0-main.py
+
+## Usage
+    - `./0-main.py`
+
+## Example
+    $ ./0-main.py
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+## Author
+    - Tofer Sokolosky
