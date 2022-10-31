132645dc69a839c63db7aa2afc3f6b3b92d20e2b
esoteric918 <3411@holbertonstudent.com>
2022-10-31 10:38:52 -0500

    refactor w/ list()/zip()

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
 def rotate_2d_matrix(matrix):
-    """Rotate a 2D matrix 90 degrees clockwise"""
-    n = len(matrix)
-    for layer in range(n // 2):
-        first = layer
-        last = n - 1 - layer
-        for i in range(first, last):
-            offset = i - first
-            top = matrix[first][i]
-            matrix[first][i] = matrix[last - offset][first]
-            matrix[last - offset][first] = matrix[last][last - offset]
-            matrix[last][last - offset] = matrix[i][last]
-            matrix[i][last] = top
-    return (matrix[first], matrix[last])
+    """Rotate a 2D matrix 90 degrees clockwise using list/zip in Python"""
+    matrix[:] = list(zip(*matrix[::-1]))
+
+
 
