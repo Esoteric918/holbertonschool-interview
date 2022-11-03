70510dba3121eff46a700746c3c51e7c4c2dd19b
esoteric918 <3411@holbertonstudent.com>
2022-11-03 10:04:00 -0500

    add comments

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+''' Rotate a 2D matrix 90 degrees clockwise using list/zip in Python '''
+
 
 def rotate_2d_matrix(matrix):
     """Rotate a 2D matrix 90 degrees clockwise using list/zip in Python"""
     matrix[:] = list(zip(*matrix[::-1]))
-
-
-
