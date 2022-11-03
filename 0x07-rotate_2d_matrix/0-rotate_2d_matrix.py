1c987bbbcf7b5575705af4618e9644e5c26b29f9
esoteric918 <3411@holbertonstudent.com>
2022-11-03 10:06:09 -0500

    list comp instead of tuple

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,4 +4,4 @@
 
 def rotate_2d_matrix(matrix):
     """Rotate a 2D matrix 90 degrees clockwise using list/zip in Python"""
-    matrix[:] = list(zip(*matrix[::-1]))
+    matrix[:] = [list(x) for x in zip(*matrix[::-1])]
