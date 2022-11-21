33e6a367b069ad112798ca68b85c9eec46c3afdf
esoteric918 <3411@holbertonstudent.com>
2022-11-21 09:46:48 -0600

    island grid

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -6,17 +6,20 @@ def makeChange(coins, total):
     '''Function to sort and count coin'''
     if total <= 0:
         return 0
-    coinlist = [0] * len(coins)
+    coinlist = [0] * (total + 1)
 
     coins.sort(reverse=True)
 
     for coin in coins:
         for i in range( coin, total + 1, coin):
-            coinlist[coins.index(coin)] += 1
-            total -= coin
-            if total == 0:
-                return sum(coinlist)
+            if coinlist[i-coin]:
+                if (coinlist[i]):
+                    coinlist[i] = min(coinlist[i], coinlist[i - coin] + 1)
+                else:
+                    coinlist[i] = coinlist[i - coin] + 1
+            elif (not i % coin):
+                coinlist[i] = i
 
-    return -1
+    return coinlist[total] if coinlist[total] else -1
 
     # return -1 if total else count
